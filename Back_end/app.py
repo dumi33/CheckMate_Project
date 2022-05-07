9778cc08ee87ab0ed137afe7d169020fe0db209d
dumi33 <dumi3345@gmail.com>
2022-05-07 13:15:32 +0900

    add cors

--- Back_end/app.py
+++ Back_end/app.py
@@ -2,11 +2,14 @@ from flask import Flask
 import students
 import classes
 import checks
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r'*': {'origins': '*'}})
 app.register_blueprint(students.blue_student)
 app.register_blueprint(classes.blue_class)
 app.register_blueprint(checks.blue_check)
+
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0", port = "8080")
