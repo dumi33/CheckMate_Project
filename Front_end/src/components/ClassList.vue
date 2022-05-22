ec96d34721541d45fbbfa07f6855b96f4e9bcf4a
남수진 <a25062401@gmail.com>
2022-05-22 13:57:11 +0900

    classlist_edit

--- Front_end/src/components/ClassList.vue
+++ Front_end/src/components/ClassList.vue
@@ -12,8 +12,7 @@
             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
             </svg>
             <button style="cursor:pointer" @click ="CaptureImage()" type = "button" id="capture_btn">캡처</button>
-            <!-- <img src='https://cdn.boan24.com/news/photo/202009/12440_11770_3815.jpg' id="cap_img"> -->
-
+            <img :src='`${img[now_img]}`' />
         </div>
         <div class = "btn_a">
         <button style="cursor:pointer" type = "button" @click="CheckStd()" id="check_btn">출석확인</button>
@@ -66,7 +65,13 @@
         CaptureImage() {
             axios.post('http://localhost:8080/checks/').then((res)=> {
                 console.log(res)
-                this.img = res.data
+            })
+            this.GetCaptureImage()
+        },
+        GetCaptureImage(){
+            axios.get('http://localhost:8080/checks/img/').then((res)=>{
+                console.log(res.data.img_url)
+                this.img = res.data.img_url
             })
         },
         // 출석체크
